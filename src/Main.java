4db6c666171f609aa448feac72db4d2363993185
tony <tonywilliamspiano@gmail.com>
2023-10-17 17:51:32 +0200

    filenotfound exception needed in signature to compile

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) throws TelegramApiException {
+    public static void main(String[] args) throws TelegramApiException, FileNotFoundException {
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         botsApi.registerBot(new ScrabbleBot());
     }
